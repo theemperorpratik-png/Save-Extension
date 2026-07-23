f9bee366cb8e05c4f4f7876203ca586791b4fd27
theemperorpratik-png <theemperorpratik@gmail.com>
2026-07-23 16:46:49 +0545

    a bit of progress

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let myLeads = ["hey","hello","hi"]
 const inputEl = document.querySelector("#input-el")
+const ulEl = document.querySelector("#ulEl")
 const inputButton = document.querySelector("#input-btn")
 inputButton.addEventListener("click", function() {
     myLeads.push(inputEl.value)
@@ -7,5 +8,10 @@ inputButton.addEventListener("click", function() {
 })
 for (let index = 0; index < myLeads.length; index++) {
     const element = myLeads[index];
-    console.log(element)
+    // console.log(element)
+    // ulEl.innerHTML += `<li>` + element + `</li>`
+    const li = document.createElement("li")
+    li.textContent = element
+    ulEl.append(li)
 }
+//hello
