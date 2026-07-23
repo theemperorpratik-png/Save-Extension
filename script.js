2df7ad0297f1d695e1cd221afed859893e7821d5
theemperorpratik-png <theemperorpratik@gmail.com>
2026-07-23 18:35:13 +0545

    almost done

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-let myLeads = ["hey","hello","hi"]
+let myLeads = []
 const inputEl = document.querySelector("#input-el")
 const ulEl = document.querySelector("#ulEl")
 const inputButton = document.querySelector("#input-btn")
-inputButton.addEventListener("click", function() {
+inputButton.addEventListener("click", function () {
     myLeads.push(inputEl.value)
     console.log(myLeads)
+    renderLead()
+    inputEl.value = ""
 })
-for (let index = 0; index < myLeads.length; index++) {
-    const element = myLeads[index];
-    // console.log(element)
-    // ulEl.innerHTML += `<li>` + element + `</li>`
-    const li = document.createElement("li")
-    li.textContent = element
-    ulEl.append(li)
+function renderLead() {
+    let listItem = `
+    <li>
+        <a href="${inputEl.value}">
+            ${inputEl.value}
+        </a>
+    </li>
+    `
+    ulEl.innerHTML += listItem
 }
-//hello
